4b5076b835bbb3fd4d436b53c639bafdcfef26b0
Fennr <fenrir1121@gmail.com>
2022-07-13 13:04:53 +0500

    Revert "fix embed permission"
    This reverts commit 444e8af9d14653b656eb813b8cdee8844f74e3b2.

--- extensions/misc.py
+++ extensions/misc.py
@@ -130,12 +130,12 @@ async def embed(ctx: ChenSlashContext) -> None:
         return
 
     if ctx.member and not helpers.includes_permissions(
-        lightbulb.utils.permissions_for(ctx.member), hikari.Permissions.SEND_MESSAGES
+        lightbulb.utils.permissions_for(ctx.member), hikari.Permissions.MANAGE_MESSAGES
     ):
         await ctx.respond(
             embed=hikari.Embed(
                 title="❌ Отсутствуют права доступа",
-                description=f"Необходимо иметь роль с правами на `Send Messages`",
+                description=f"Необходимо иметь роль с правами на `Manage Messages`",
                 color=const.ERROR_COLOR,
             ),
             flags=hikari.MessageFlag.EPHEMERAL,
